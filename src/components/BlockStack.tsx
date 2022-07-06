79d1535e5c25eb594de544ee5869804af17e1e7d
Neel Dutta <neeldutta@neels-air.lan>
2022-07-06 16:47:42 +1000

    Some changes to cat section

--- src/components/BlockStack.tsx
+++ src/components/BlockStack.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Button } from "theme-ui";
-import { theme } from "../theme";
 import Block from "./Block";
 
 interface Props {}
--- src/components/CatSection.tsx
+++ src/components/CatSection.tsx
@@ -1,11 +1,17 @@
+import styled from "@emotion/styled";
 import React from "react";
 
 interface Props {}
 
 const CatSection = (props: Props) => {
   const {} = props;
-
-  return <div className="CatSection"></div>;
+  const StyledCatSection = styled.div`
+  
+  
+  `
+  return (
+    <StyledCatSection></StyledCatSection>
+  );
 };
 
 export default CatSection;
