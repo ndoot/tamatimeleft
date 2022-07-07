3267354e403b4a9c51f22f8bded6ee201cbd2763
Neel Dutta <neeldutta@neels-air.lan>
2022-07-07 22:51:15 +1000

    Added cat section, cats based on report

--- src/components/CatSection.tsx
+++ src/components/CatSection.tsx
@@ -2,6 +2,11 @@ import styled from "@emotion/styled";
 import React from "react";
 import { Heading } from "theme-ui";
 import { FinanceReport } from "../interfaces";
+import happycat from './assets/happycat.gif'; 
+import sadcat from './assets/sadcat.gif'; 
+import sleepcat from './assets/sleepcat.gif'; 
+
+
 
 interface Props {
   report: FinanceReport;
@@ -12,18 +17,24 @@ const CatSection = (props: Props) => {
   const { report } = props;
 
   const StyledCatSection = styled.div`
-  
-  
+    height: 500px;
+    align-items: center;
+    background-color: ${(props) => props.theme.colors?.highlight};
+    border-radius: 25px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   `
+  const [heading, cat] = (report.dying === undefined
+    ? ["Enter your current finances", sleepcat]
+    : report.dying
+    ? [`You have ${report.daysToLive} days to live`, sadcat]
+    : ["Congrats your pet is thriving!", happycat]);
+
   return (
     <StyledCatSection>
-      <Heading as="h2">
-        {report.dying === undefined
-          ? "Enter your current finances"
-          : report.dying
-          ? `You have ${report.daysToLive} days to live`
-          : "Congrats your pet is thriving!"}
-      </Heading>
+      <Heading as="h2">{heading}</Heading>
+      <img src={cat} alt="cat" width={300} height={300}></img>
     </StyledCatSection>
   )
 };
